14934e9559fb3000a3c0f15561d51421d14b3417
Роман Бандурин <roman.obj@mail.ru>
2024-09-09 07:30:26 +0300

    dddd

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { URL, fileURLToPath } from "node:url"
+import { defineConfig } from "vite"
+import injectHTML from 'vite-plugin-html-inject'
+
+export default defineConfig({
+  plugins: [injectHTML()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+      },
+      output: {
+        manualChunks: undefined,
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  server: {
+    open: "index.html",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./", import.meta.url)),
+    },
+  }
+})
